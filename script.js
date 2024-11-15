33dfd6d68279be7eef8c8926d59661ae6a977246
Priyansh Garg <priyanshg433@gmail.com>
2024-11-15 15:58:25 +0530

    Code

---
+++ script.js
@@ -0,0 +1,21 @@
+function inputValue(value) {
+    document.getElementById('display').innerText += value;
+}
+
+function clearDisplay() {
+    document.getElementById('display').innerText = '';
+}
+
+function deleteLast() {
+    const display = document.getElementById('display');
+    display.innerText = display.innerText.slice(0, -1);
+}
+
+function calculateResult() {
+    const display = document.getElementById('display');
+    try {
+        display.innerText = eval(display.innerText);
+    } catch {
+        display.innerText = 'Error';
+    }
+}
